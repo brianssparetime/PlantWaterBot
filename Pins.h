cc11948d6a3076c7ad0dff53aff8d5e81adbc098
Brian <brianssparetime@gmail.com>
2022-06-19 18:44:26 -0700

    bring pins into alignment with darkroom timer pcb

--- Pins.h
+++ Pins.h
@@ -1,4 +1,12 @@
-#define RELAY_PIN 5
+#pragma once
+#include "Arduino.h"
+// to match PCB from darkroom timer
+#define RELAY_PIN A0 
+
 #define RE_BUT_PIN 6
 #define RE_A_PIN 7
 #define RE_B_PIN 8
+
+// LCD uses I2C SDA / SCL
+
+// 
