92e7bc4186bb9070a011d7a9582b3a072c95faaa
Brian <brianssparetime@gmail.com>
2022-08-11 00:02:10 -0700

    Revert "changes to avoid usage of String"
    This reverts commit 205543fe9402b3b4acb73b38610ca77b6f0a2ad6.

--- plant_water_bot/LCD_Wrapper.h
+++ plant_water_bot/LCD_Wrapper.h
@@ -10,9 +10,7 @@ class LCD_Wrapper {
         static void backlight();
         static void backlightOff();
         static void update();
-        static void display(char* line1, char* line2);
-        static void display(const __FlashStringHelper* line1, const __FlashStringHelper* line2);
-        static void display(const __FlashStringHelper* line1, char* line2);
+        static void display(String line1, String line2);
 
     private:
         static LiquidCrystal_I2C* _lcd;
